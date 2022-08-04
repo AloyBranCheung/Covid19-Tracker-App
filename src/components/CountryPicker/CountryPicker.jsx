bd446f090f7102ca8cc7bb130c77efd408b045d3
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-08-04 15:33:31 -0400

    selector fix

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import NativeSelect from "@mui/material/NativeSelect";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import styles from "./CountryPicker.module.css";
 import { fetchCountries } from "../../api";
@@ -26,9 +27,9 @@ export default function CountryPicker({
 
   const mappedCountries = countries.sort().map((country) => {
     return (
-      <option key={uuidv4()} value={country}>
+      <MenuItem key={uuidv4()} value={country}>
         {country}
-      </option>
+      </MenuItem>
     );
   });
 
@@ -39,15 +40,14 @@ export default function CountryPicker({
     });
     const finalValue = selectedCountry[0].slug;
     onHandleCountryChange(finalValue);
-    return finalValue;
   };
 
   return (
     <FormControl className={styles.formControl}>
-      <NativeSelect defaultValue="" onChange={nativeSelectChangeHandler}>
-        <option value="canada">Canada</option>
+      <Select defaultValue="Canada" onChange={nativeSelectChangeHandler}>
+        <MenuItem value="Canada">Canada</MenuItem>
         {mappedCountries}
-      </NativeSelect>
+      </Select>
       <Button onClick={toggleHandler} variant="contained">
         Toggle
       </Button>
